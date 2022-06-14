e46bfb32b5f20e682d917e5a58e262074003ed14
Isaac <isaac@talaios.coop>
2022-06-14 23:51:43 +0200

    Jatos test

--- storage/app/base-build/src/App.vue
+++ storage/app/base-build/src/App.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="main">
         <main class="content">
+            <NavBar />
             <div class="container-fluid p-0">
                 <router-view />
             </div>
         </main>
     </div>
 </template>
+
+<script setup>
+//import NavBar from '../../../../resources/js/Components/NavBar.vue';
+import NavBar from './components/NavBar.vue';
+</script>
---
+++ storage/app/base-build/src/components/NavBar.vue
@@ -0,0 +1,64 @@
+<template>
+    <!-- <nav class="nav nav-pills nav-justified">
+        <router-link
+            to="/"
+            class="nav-item nav-link"
+            :class="{ active: active === 'dashboard' }"
+            @click="setActive('dashboard')"
+            >Dashboard</router-link
+        >
+        <router-link
+            to="/builder"
+            class="nav-item nav-link"
+            :class="{ active: active === 'other' }"
+            @click="setActive('other')"
+            >Active</router-link
+        >
+    </nav> -->
+    <nav class="nav nav-pills nav-justified">
+        <router-link
+            to="/"
+            class="nav-item nav-link"
+            :class="{ active: active === 'dashboard' }"
+            @click="setActive('dashboard')"
+            >Dashboard</router-link
+        >
+        <a
+            href="#"
+            class="nav-item nav-link"
+            :class="{ active: active === 'input' }"
+            @click="setActive('input')"
+            >Input</a
+        >
+        <a
+            href="#"
+            class="nav-item nav-link"
+            :class="{ active: active === 'image' }"
+            @click="setActive('image')"
+            >Image</a
+        >
+        <a
+            href="#"
+            class="nav-item nav-link"
+            :class="{ active: active === 'video' }"
+            @click="setActive('video')"
+            >Video</a
+        >
+        <a
+            href="#"
+            class="nav-item nav-link"
+            :class="{ active: active === 'text' }"
+            @click="setActive('text')"
+            >Text</a
+        >
+    </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const active = ref('');
+const setActive = (item) => {
+    active.value = item;
+};
+</script>
